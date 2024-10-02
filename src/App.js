c6826f0b5c0c8f9ecf95736ce87b4a21072f805c
Micah <contact_micah@yahoo.com>
2024-10-02 09:51:31 -0700

    Oct 02, 24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Sep-29-24
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {                                 
+                const response = await fetch('http://ec2-54-188-14-185.us-west-2.compute.amazonaws.com:8000/data');// <your-ec2-public-ip>
+                                              
+                if (!response.ok) {
+                    throw new Error('Network response was not ok!');
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+        
+            <h1>Data from FastAPI</h1>
+            {data ? (
+                <div>
+                    <p>Feature 1: {data.feature1}</p>
+                    <p>Feature 2: {data.feature2}</p>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
 
 export default App;
+
